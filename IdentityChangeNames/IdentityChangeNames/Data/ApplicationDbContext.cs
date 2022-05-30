761c1c387c5c82ba6822cd324012e459bbc3a50c
THALICE SILVA <2020101106@academicougb.com.br>
2022-05-30 17:59:02 -0300

    Inserção de projeto com modificações nas tabelas padrão criadas pelo Identity

---
+++ IdentityChangeNames/IdentityChangeNames/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using IdentityChangeNames.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityChangeNames.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>().ToTable("User");
+            builder.Entity<ApplicationRole>().ToTable("Role");
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRole");
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogin");
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaim");
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserToken");
+            builder.Entity<ApplicationUser>(b =>
+            {
+                b.Property(au => au.Email).HasColumnName("EmailAddress");
+            });
+        }
+    }
+}
